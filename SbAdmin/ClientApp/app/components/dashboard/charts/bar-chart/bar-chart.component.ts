03ee0d0e6daa10b9d3975bd573805657398146c1
Dmytro <dmytro.tsiniavskyi@gmail.com>
2018-02-28 17:56:27 +0200

    implemented area and bar chart components

--- SbAdmin/ClientApp/app/components/dashboard/charts/bar-chart/bar-chart.component.ts
+++ SbAdmin/ClientApp/app/components/dashboard/charts/bar-chart/bar-chart.component.ts
@@ -1,12 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+declare var Morris: any;
 
 @Component({
     selector: 'bar-chart',
     templateUrl: './bar-chart.component.html'
 })
 export class BarChartComponent implements OnInit {
+    data: {
+      y: string;
+      a: number;
+      b: number;
+    }[];
 
     ngOnInit() {
-      
+      this.data = [{
+        y: '2006',
+        a: 100,
+        b: 90
+      }, {
+        y: '2007',
+        a: 75,
+        b: 65
+      }, {
+        y: '2008',
+        a: 50,
+        b: 40
+      }, {
+        y: '2009',
+        a: 75,
+        b: 65
+      }, {
+        y: '2010',
+        a: 50,
+        b: 40
+      }, {
+        y: '2011',
+        a: 75,
+        b: 65
+      }, {
+        y: '2012',
+        a: 100,
+        b: 90
+      }];
+
+      Morris.Bar({
+        element: 'morris-bar-chart',
+        data: this.data,
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Series A', 'Series B'],
+        hideHover: 'auto',
+        resize: true
+      });
     }
 }
