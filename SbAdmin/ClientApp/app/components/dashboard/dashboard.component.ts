1fbfe82887734f13e97490f75fbaf79d3171fc0d
Dmytro Tsiniavskyi <dmytro.tsiniavskyi@hotmail.com>
2018-02-28 02:58:08 +0200

    refactor dahsboard component

--- SbAdmin/ClientApp/app/components/dashboard/dashboard.component.ts
+++ SbAdmin/ClientApp/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'dashboard',
     templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+    badges: {
+        count: number,
+        icon: string,
+        caption: string,
+        color: string
+    }[] = []
+
+    ngOnInit() {
+        this.badges.push({ count: 100, icon: 'fa-comments', caption: 'New Comments!', color: 'primary' });
+        this.badges.push({ count: 100, icon: 'fa-tasks', caption: 'New Tasks!', color: 'green' });
+        this.badges.push({ count: 100, icon: 'fa-shopping-cart', caption: 'New Orders!', color: 'yellow' });
+        this.badges.push({ count: 100, icon: 'fa-support', caption: 'Support Tickets!', color: 'red' });
+    }
 }
